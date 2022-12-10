71e0bee0f0d2e8241af94ef9914471d852e6620c
katecameronsmith <105184752+katecameronsmith@users.noreply.github.com>
2022-12-10 16:07:49 +1030

    Reviewed Update

--- app.js
+++ app.js
@@ -11,17 +11,17 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 
 /**global navigation variables  */
 const navigation = document.getElementById('navbar__list');
@@ -33,7 +33,7 @@ const sections = document.querySelectorAll('section');
  * End Global Variables
  * Start Helper Functions
  *
-*/
+ */
 
 
 
@@ -41,28 +41,27 @@ const sections = document.querySelectorAll('section');
  * End Helper Functions
  * Begin Main Functions
  *
-*/
+ */
 
 /** build the nav */
 function navBuild() {
-    function setActiveClass(e){
-        var currentSection = e.target.innerHTML;
-        var selectedSections = Array.from(document.getElementsByTagName("section"));
-        for (var current in selectedSections) {
-            if (selectedSections[current].dataset.nav === currentSection){
+    function setActiveClass(e) {
+        const currentSection = e.target.innerHTML;
+        const selectedSections = Array.from(document.getElementsByTagName("section"));
+        for (const current in selectedSections) {
+            if (selectedSections[current].dataset.nav === currentSection) {
                 selectedSections[current].setAttribute('class', "your-active-class");
-            }
-            else{
+            } else {
                 selectedSections[current].setAttribute('class', "");
             }
         }
-        
+
     }
-    var selectedSections = Array.from(document.getElementsByTagName("section"));
+    const selectedSections = Array.from(document.getElementsByTagName("section"));
     // section loop
-    for (var current in selectedSections) {
-        var sectionID = selectedSections[current].id;
-        var sectionDataNav = selectedSections[current].dataset.nav;
+    for (const current in selectedSections) {
+        const sectionID = selectedSections[current].id;
+        const sectionDataNav = selectedSections[current].dataset.nav;
         // create the li element and add to the navigation bar */
         const newLi = document.createElement('li');
         navigation.appendChild(newLi);
@@ -72,19 +71,19 @@ function navBuild() {
         navButton.setAttribute('href', `#${sectionID}`);
         navButton.textContent = sectionDataNav;
         newLi.appendChild(navButton);
-        navButton.addEventListener('click', function (e) {
+        navButton.addEventListener('click', function(e) {
             setActiveClass(e)
         });
     }
 }
 
 function callback(e) {
-    var e = window.e || e;
-    var selectedSections = Array.from(document.getElementsByTagName("section"));
+    const e = window.e || e;
+    const selectedSections = Array.from(document.getElementsByTagName("section"));
     console.log(sections[0])
     console.log(e.target.attributes.href.value)
-    for (var current in selectedSections) {
-        selectedSections[current].class="";
+    for (const current in selectedSections) {
+        selectedSections[current].class = "";
     }
 }
 
@@ -93,14 +92,14 @@ navBuild();
 console.log('test')
 
 // Scroll to section on link click
-let elem = document.querySelector('div');
-let rect = elem.getBoundingClientRect();
+const elem = document.querySelector('div');
+const rect = elem.getBoundingClientRect();
 for (const key in rect) {
-  if (typeof rect[key] !== 'function') {
-    let para = document.createElement('p');
-    para.textContent = `${key} : ${rect[key]}`;
-    document.body.appendChild(para);
-  }
+    if (typeof rect[key] !== 'function') {
+        let para = document.createElement('p');
+        para.textContent = `${key} : ${rect[key]}`;
+        document.body.appendChild(para);
+    }
 }
 
 
@@ -109,26 +108,24 @@ for (const key in rect) {
 window.addEventListener('scroll', (e) => {
 
     // get all sections on the page
-    var sections = Array.from(document.getElementsByTagName("section"));
-  
+    const sections = Array.from(document.getElementsByTagName("section"));
+
     // loop through each section
-    sections.forEach( section => {
-  
-      // get px distance from top
-      const topDistance = section.getBoundingClientRect().top;
-  
-      // if the distance to the top is between 0-100px
-      if (topDistance > -50 && topDistance < 150) {
-        section.classList.add('your-active-class');
-      }
-      // otherwise, remove the class
-      
-      
-      else {
-        section.classList.remove('your-active-class');
-      }
+    sections.forEach(section => {
+
+        // get px distance from top
+        const topDistance = section.getBoundingClientRect().top;
+
+        // if the distance to the top is between 0-100px
+        if (topDistance > -50 && topDistance < 150) {
+            section.classList.add('your-active-class');
+        }
+        // otherwise, remove the class
+        else {
+            section.classList.remove('your-active-class');
+        }
     });
-  });
+});
 
 
 // Shows which section is in viewport
@@ -143,7 +140,7 @@ window.addEventListener('scroll', (e) => {
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 
 // Build menu
 
